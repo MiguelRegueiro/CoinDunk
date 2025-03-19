a9b26d59258bbe0ff74d9fdbe0979c7dde7c792c
MiguelRegueiro <miguelpr4242@gmail.com>
2025-03-19 10:14:55 +0100

    Navbar arreglado bordes y logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,35 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <AppBar
-      position="static"
+      position="fixed" // Cambia 'static' a 'fixed'
       sx={{
         backgroundColor: '#2c3e50', // Color de fondo oscuro
         color: '#fff', // Texto blanco
         boxShadow: 'none', // Sin sombra
         borderBottom: '1px solid #34495e', // Borde inferior sutil
+        width: '100vw', // Asegura que ocupe todo el ancho de la ventana
+        margin: 0, // Elimina cualquier margen
+        padding: 0, // Elimina cualquier padding
+        left: 0, // Asegura que esté pegado a la izquierda
+        right: 0, // Asegura que esté pegado a la derecha
+        top: 0, // Asegura que esté pegado a la parte superior
+        zIndex: 1200, // Asegura que esté por encima de otros elementos
+        height: '60px', 
+        justifyContent: 'center',
       }}
     >
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <Toolbar sx={{ 
+        display: 'flex', 
+        justifyContent: 'space-between', 
+        alignItems: 'center',
+        maxWidth: '1200px', // Limita el ancho del contenido
+        width: '100%', // Asegura que el Toolbar ocupe todo el ancho disponible
+        margin: '0 auto', // Centra el contenido
+        padding: '0 16px', // Añade un poco de padding a los lados
+      }}>
         {/* Logo y nombre de la aplicación */}
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <img src="/CoinDunkNB.png" alt="CoinDunk Logo" style={{ height: 52, width: 58, marginRight: 2 }} />
           <Typography variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
             CoinDunk
           </Typography>
